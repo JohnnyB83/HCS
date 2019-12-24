fd0796d2f8ff1cc6ab401bd44d1250edb1defd00
Wes Byers <wbyers@box.com>
2019-12-24 15:07:12 -0800

    add analog clock to settings pane add base styling for info on settings pane

--- client/src/SystemStatus/SystemStatus.tsx
+++ client/src/SystemStatus/SystemStatus.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import './styles/SystemStatus.css';
 
+import AnalogClock, { Themes } from 'react-analog-clock';
+
 type SystemStatusProps = {
     timeSystemHasBeenOn: Date,
     totalTimeSystemWillBeOn: Date,
@@ -23,16 +25,20 @@ const SystemStatus: FunctionComponent<SystemStatusProps> = ({
     return (
         <div className='SystemStatus'>
             <div className='SystemStatus-clock'>
+                <AnalogClock theme={Themes.dark} width={200}/>
+            </div>
+            <div className='SystemStatus-info'>
+                <div className='SystemStatus-lightCycle-icon'>sun</div>
+                <div className='SystemStatus-lightCycle-time'>5:32 / 17</div>
+                <div className='SystemStatus-lightCycle-plantState'>plant</div>
 
-            </div>
-            <div className='SystemStatus-icons'>
-                
-            </div>
-            <div className='SystemStatus-timeData'>
-                
-            </div>
-            <div className='SystemStatus-stateData'>
-                
+                <div className='SystemStatus-reservoir-icon'>reservoir</div>
+                <div className='SystemStatus-reservoir-time'>5 days</div>
+                <div className='SystemStatus-reservoir-amount'>15 Gal</div>
+
+                <div className='SystemStatus-nutrients-icon'>nutrients</div>
+                <div className='SystemStatus-nutrients-time'>5 days</div>
+                <div className='SystemStatus-nutrients-amount'>15 Tbsp</div>
             </div>
         </div>
     )
