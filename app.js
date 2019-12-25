33afccce35d178c902e02ea200145d275fa4f74b
byersrwn <58962621+byersrwn@users.noreply.github.com>
2019-12-25 00:17:44 -0800

    basic web server components started & plant&HCS config json created

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-const RPI = require('./RPI/RaspberryPiFunctions');
+// const RPI = require('./RPI/RaspberryPiFunctions');
+
+// cron.schedule("* * * * *", function() {
+//     console.log("running a task every minute");
+//   });
+
+const app = require("express")();
+const bodyParser = require("body-parser");
+const fs = require("fs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+app.get("/", function(req, res) {
+    console.log("get");
+    let data = fs.readFileSync('HCSConfig&PlantData.json');
+    let pd = JSON.parse(data);
+    res.send(pd);
+});
+
+app.listen(5000, function() {
+    console.log("started on port 5000");
+
+});
+
